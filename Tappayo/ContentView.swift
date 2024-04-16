599a90f9986a281c243fbb4c8963a14f320af357
markitics <m@rkmoriarty.com>
2024-04-16 02:18:35 -0700

    Initial Commit

---
+++ Tappayo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tappayo
+//
+//  Created by M@rkMoriarty.com on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tappayo/TappayoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TappayoApp.swift
+//  Tappayo
+//
+//  Created by M@rkMoriarty.com on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TappayoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
