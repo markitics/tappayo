1867fcc4ee9cd319c55f45d1b0285c5fde58ca85
markitics <m@rkmoriarty.com>
2025-11-18 19:28:58 -0800

    Clean up: delete three dead files from 2024: basketview.swift, checkoutview.swift and the useless AppStorageArray.swift

--- Tappayo/UserDefaultsKeys.swift
+++ Tappayo/UserDefaultsKeys.swift
@@ -2,15 +2,6 @@
 //  Tappayo
 //  Created by M@rkMoriarty.com
 
-//import Foundation
-//
-//struct UserDefaultsKeys {
-//    static let quickAmounts = "quickAmounts"
-//    static let accentColor = "accentColor"
-//    static let darkModePreference = "darkModePreference"
-//}
-
-
 import Foundation
 
 enum UserDefaultsKeys: String {
